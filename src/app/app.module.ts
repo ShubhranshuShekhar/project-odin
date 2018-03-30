7f504c0fbf4245b53ddd9e089eb91fb2c70bdf01
ShekharShubh <shubhranshu.shekhar.cat@gmail.com>
2018-03-30 12:16:22 +0530

    Installed Theme
    https://themeforest.net/item/mega-able-bootstrap-4-and-angular-5-admin-dashboard-template/20790784

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { AdminComponent } from './layout/admin/admin.component';
+import { AuthComponent } from './layout/auth/auth.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {SharedModule} from './shared/shared.module';
+import {MenuItems} from './shared/menu-items/menu-items';
+import {BreadcrumbsComponent} from './layout/admin/breadcrumbs/breadcrumbs.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AdminComponent,
+    AuthComponent,
+    BreadcrumbsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    SharedModule
   ],
-  providers: [],
+  providers: [MenuItems],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
