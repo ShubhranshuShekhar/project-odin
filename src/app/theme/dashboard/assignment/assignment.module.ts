29c597f20bdeee1ea348ca42b3c7c2afe31cdaf7
ShekharShubh <shubhranshu.shekhar.cat@gmail.com>
2018-04-03 00:32:46 +0530

    assignment is working

--- src/app/theme/dashboard/assignment/assignment.module.ts
+++ src/app/theme/dashboard/assignment/assignment.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AssignmentComponent } from './assignment.component';
 import { AssignmentRoutingModule } from './assignment-routing.module';
+import { SharedModule } from '../../../shared/shared.module';
 
 @NgModule({
   imports: [
     CommonModule,
-    AssignmentRoutingModule
+    AssignmentRoutingModule,
+    SharedModule
   ],
   declarations: [AssignmentComponent],
   bootstrap: [AssignmentComponent]
